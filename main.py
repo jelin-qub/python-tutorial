1d14890fe3c0e436b46d790fa87afc6083662081
jelin-qub <jelin@qubryx.com>
2023-10-06 15:17:15 +0530

    exceptions

--- main.py
+++ main.py
@@ -14,47 +14,70 @@ def ensure_table(engine, table_name, headers):
         columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
         table = db.Table(table_name, metadata, *columns)
         metadata.create_all(engine)
+        try:
+            metadata.create_all(engine)
+        except Exception as e:
+            print(f"An error occurred while creating the table {table_name}: {e}")
     else:
         print(table_name + ' ' + "exist")
 
 # Parsing table from URL which will return the parsed table as an array of rows
 def parse_table(url):
-    r = requests.get(url)
-    s = BeautifulSoup(r.content, 'html.parser')
-    table = s.find("table", class_='wikitable sortable')
+    try:
+            r = requests.get(url)
+            s = BeautifulSoup(r.content, 'html.parser')
+            table = s.find("table", class_='wikitable sortable')
 
-    # Extract headers
-    header_row = table.find("tr", class_='is-sticky')
-    headers = [header.text.strip().lower() for header in header_row.find_all(["th", "td"])]
-    titles = header_row.get_text().strip().replace('\n', ',')
+            # Extract headers
+            header_row = table.find("tr", class_='is-sticky')
+            if header_row:
+                headers = [header.text.strip().lower() for header in header_row.find_all(["th", "td"])]
+                print(headers)
+                titles = header_row.get_text().strip().replace('\n', ',')
+                print(titles)
 
-    # Datas of each row
-    data = []
-    for row in table.find_all("tr")[1:]:
-        # Extract text from each cell in the row
-        cells = row.find_all(["td", "th"])
-        row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
-        data.append(row_data)
-        for i in data:
-            print(i)
+                # Datas of each row
+                data = []
+                for row in table.find_all("tr")[1:]:
+                    # Extract text from each cell in the row
+                    cells = row.find_all(["td", "th"])
+                    row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
+                    data.append(row_data)
+                for row in data:
+                    country = row.get('country / dependency')  # Adapt to the actual headers in your table
+                    population = row.get('population')
+                    print(f'Country: {country},   Population: {population}')
 
-    return headers, data
+                return headers, data
+            else:
+                raise ValueError("Header row not found in the table.")
+    except Exception as e:
+        print(f"An error occurred while parsing the table: {e}")
+
+    # Return None if an error occurred
+    return None, None
+
 
 # Function to store data in a dynamically created table in the SQLite database
-def storing_to_table(engine, list_of_items, table_name, headers):
-    # Create the table dynamically based on headers
-    columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
+def storing_to_table(engine, data, table_name, headers):
+    try:
+        # Extract 'Country' and 'Population' from each row
+        country_population_data = [{'country': row.get('country / dependency'), 'population': row.get('population')} for row in data]
 
-    table = db.Table(table_name, metadata, *columns)
-    metadata.create_all(engine)
+        # Create the table dynamically based on headers
+        columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
+        table = db.Table(table_name, metadata, *columns)
+        metadata.create_all(engine)
 
-    # Insert data into the table
-    conn.execute(table.insert(), list_of_items)
-
+        # Insert data into the table
+        conn.execute(table.insert(), country_population_data)
+        print("Data inserted into table '{table_name}' successfully.")
+    except Exception as e:
+        print(f"An error occurred while storing data to the table: {e}")
 # Ensure the table exists in the database
 url = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
 headers, data = parse_table(url)
 ensure_table(engine, 'countries_list', headers)
 
 # Store data in the database table
-storing_to_table(engine, data, 'countries_list', headers)
+storing_to_table(engine, data, 'countries_list',headers)
