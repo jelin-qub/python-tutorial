5ddd391758b81ef6093b7acd2c7306c8618efb45
jelin-qub <jelin@qubryx.com>
2023-10-06 10:53:14 +0530

    parsing_storing_ensuring

--- main.py
+++ main.py
@@ -3,36 +3,72 @@ engine = db.create_engine('sqlite:///datacamp.sqlite')
 conn = engine.connect()
 metadata = db.MetaData()
 
-l1=[{'country':'india','population':50000},{'country':'nepal','population':40000}]
-def store_to_table(engine,list_of_items,table_name):
-    table_name = db.Table(table_name, metadata,
-                         db.Column('country', db.String(255), primary_key=True),
-                         db.Column('population', db.Integer(), nullable=False)
-                         )
+import requests
+from bs4 import BeautifulSoup
+
+# beautifulsoup scrapping table -----
+r=requests.get('https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population')
+s=BeautifulSoup(r.content,'html.parser')
+
+table=s.find("table",class_='wikitable sortable')
+# print(table)
+
+# Extract headers
+header_row = table.find("tr", class_='is-sticky')
+headers = [header.text.strip().lower() for header in header_row.find_all(["th", "td"])]
+titles = header_row.get_text().strip().replace('\n', ',')
+# datas of each rows
+data = []
+for row in table.find_all("tr")[1:]:
+    # Extract text from each cell in the row
+    cells = row.find_all(["td", "th"])
+    row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
+    data.append(row_data)
+
+
+#parsing table from url which will return the parsed table as array of rows
+def parse_table(table):
+    titles = header_row.get_text().strip().replace('\n', ',')
+    print([titles])
+    #datas of each rows
+    data=[]
+    for row in table.find_all("tr")[1:]:
+        # Extract text from each cell in the row
+        cells = row.find_all(["td", "th"])
+        row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
+        data.append(row_data)
+    for i in data:
+        print(i)
+
+table = s.find("table", class_='wikitable sortable')
+parse_table(table)
+
+
+# Function to store data in a dynamically created table in the SQLite database
+def storing_to_table(engine,list_of_items,table_name,headers):
+    # Create the table dynamically based on headers
+    columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
+
+    table = db.Table(table_name, metadata, *columns)
     metadata.create_all(engine)
 
-    for i in list_of_items:
-        query = db.insert(table_name).values(country=i['country'], population=i['population'])
-        conn.execute(query)
-    select_query = table_name.select()
-    result = conn.execute(select_query).fetchall()
-    print(result)
-    conn.close()
-store_to_table(engine,l1,'countries')
+    # Insert data into the tablez
+    conn.execute(table.insert(), list_of_items)
+
+storing_to_table(engine, data, 'countries_list',headers)
+
+
+
 
 def ensure_table(engine,table_name):
     inspector = db.inspect(engine)
 
     if not inspector.has_table(table_name):
         metadata = db.MetaData()
-        table = db.Table(table_name, metadata,
-                         db.Column('country', db.String(255), primary_key=True),
-                         db.Column('population', db.Integer(), nullable=False)
-                         )
+        columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
+        table = db.Table(table_name, metadata, *columns)
         metadata.create_all(engine)
     else:
         print(table_name + ' ' + "exist")
 
-ensure_table(engine,'countries1')
-ensure_table(engine,'countries')
-ensure_table(engine,'apple')
+ensure_table(engine,'countries_list')
