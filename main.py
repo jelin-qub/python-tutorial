6e1979c0c16ac0469ecf63209841f48cdafb4817
jelin-qub <jelin@qubryx.com>
2023-10-06 12:04:07 +0530

    single script

--- main.py
+++ main.py
@@ -1,66 +1,12 @@
 import sqlalchemy as db
+import requests
+from bs4 import BeautifulSoup
+
 engine = db.create_engine('sqlite:///datacamp.sqlite')
 conn = engine.connect()
 metadata = db.MetaData()
 
-import requests
-from bs4 import BeautifulSoup
-
-# beautifulsoup scrapping table -----
-r=requests.get('https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population')
-s=BeautifulSoup(r.content,'html.parser')
-
-table=s.find("table",class_='wikitable sortable')
-# print(table)
-
-# Extract headers
-header_row = table.find("tr", class_='is-sticky')
-headers = [header.text.strip().lower() for header in header_row.find_all(["th", "td"])]
-titles = header_row.get_text().strip().replace('\n', ',')
-# datas of each rows
-data = []
-for row in table.find_all("tr")[1:]:
-    # Extract text from each cell in the row
-    cells = row.find_all(["td", "th"])
-    row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
-    data.append(row_data)
-
-
-#parsing table from url which will return the parsed table as array of rows
-def parse_table(table):
-    titles = header_row.get_text().strip().replace('\n', ',')
-    print([titles])
-    #datas of each rows
-    data=[]
-    for row in table.find_all("tr")[1:]:
-        # Extract text from each cell in the row
-        cells = row.find_all(["td", "th"])
-        row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
-        data.append(row_data)
-    for i in data:
-        print(i)
-
-table = s.find("table", class_='wikitable sortable')
-parse_table(table)
-
-
-# Function to store data in a dynamically created table in the SQLite database
-def storing_to_table(engine,list_of_items,table_name,headers):
-    # Create the table dynamically based on headers
-    columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
-
-    table = db.Table(table_name, metadata, *columns)
-    metadata.create_all(engine)
-
-    # Insert data into the tablez
-    conn.execute(table.insert(), list_of_items)
-
-storing_to_table(engine, data, 'countries_list',headers)
-
-
-
-
-def ensure_table(engine,table_name):
+def ensure_table(engine, table_name, headers):
     inspector = db.inspect(engine)
 
     if not inspector.has_table(table_name):
@@ -71,4 +17,44 @@ def ensure_table(engine,table_name):
     else:
         print(table_name + ' ' + "exist")
 
-ensure_table(engine,'countries_list')
+# Parsing table from URL which will return the parsed table as an array of rows
+def parse_table(url):
+    r = requests.get(url)
+    s = BeautifulSoup(r.content, 'html.parser')
+    table = s.find("table", class_='wikitable sortable')
+
+    # Extract headers
+    header_row = table.find("tr", class_='is-sticky')
+    headers = [header.text.strip().lower() for header in header_row.find_all(["th", "td"])]
+    titles = header_row.get_text().strip().replace('\n', ',')
+
+    # Datas of each row
+    data = []
+    for row in table.find_all("tr")[1:]:
+        # Extract text from each cell in the row
+        cells = row.find_all(["td", "th"])
+        row_data = {header.lower(): cell.get_text(strip=True) for header, cell in zip(headers, cells)}
+        data.append(row_data)
+        for i in data:
+            print(i)
+
+    return headers, data
+
+# Function to store data in a dynamically created table in the SQLite database
+def storing_to_table(engine, list_of_items, table_name, headers):
+    # Create the table dynamically based on headers
+    columns = [db.Column(header.lower(), db.String(255)) for header in headers if header.strip()]
+
+    table = db.Table(table_name, metadata, *columns)
+    metadata.create_all(engine)
+
+    # Insert data into the table
+    conn.execute(table.insert(), list_of_items)
+
+# Ensure the table exists in the database
+url = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
+headers, data = parse_table(url)
+ensure_table(engine, 'countries_list', headers)
+
+# Store data in the database table
+storing_to_table(engine, data, 'countries_list', headers)
